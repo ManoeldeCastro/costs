0d2fd3200f94a802046b4565473a8f61e52bba7c
manoel <manoeldiasmf@outlook.com>
2023-01-05 08:00:03 -0300

    Edit_Project_Card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Company from './components/pages/Company';
 import Contato from './components/pages/Contato';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import Projects from './components/pages/Projects';
 
 function App() {
   return (
@@ -21,6 +23,7 @@ function App() {
           <Route path="/company" element={<Company />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />

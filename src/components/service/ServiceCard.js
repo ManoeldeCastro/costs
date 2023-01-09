6fa0bfaa0f0254eb150ac64a2549e625820f31e5
manoel <manoeldiasmf@outlook.com>
2023-01-08 22:06:27 -0300

    Update ServiceCard.js

--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -5,7 +5,7 @@ function ServiceCard({ id, name, cost, description, handleRemove}){
     <div className={styles.project_card}>
       <h4>{name}</h4>
       <p>
-        <span>Custo total:</span> R$ {cost}
+        <span>Custo total:</span> R${cost}
       </p>
     </div>
   )
